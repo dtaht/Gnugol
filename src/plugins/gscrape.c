5761b1f8a84f75e1a7fb94ff0f10d8beabf5a390
David Taht <d@teklibre.com>
2008-02-28 19:32:21 -0600

    Proto working slightly better

--- src/plugins/gscrape.c
+++ src/plugins/gscrape.c
@@ -1,5 +1,5 @@
 /* program sends user enter message from parent to child and back */
-/* contributed by david rowe. */
+/* core contributed by david rowe. */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,29 +8,30 @@
 #include <fcntl.h>
 #include <errno.h>
 
+#include "common.h"
+
 #define EXECV
-int gnugol_plugin_gscrape(argc, argv)
-     int argc;
-     char *argv[];
+
+int gnugol_plugin_gscrape_init(int p2p[2]) // I'm confused, this is an array ref?
 {
     int i, pid, p2c[2], c2p[2];
     char send[80], receive[80], *gets();
  
     if ( pipe(p2c) != 0) {
 	printf("p2c pipe creation error");
-	exit(1);
+	return(-1);
     }
 
     if ( pipe(c2p) != 0) {
 	printf("c2p pipe creation error");
-	exit(1);
+	return(-1);
     }
 
     i = 0;
     pid = fork();
     if (pid == -1) {
 	printf("fork error\n");
-	exit(0);
+	return(pid);
     }
  
     if (pid == 0) {                        /* CHILD process */
@@ -38,7 +39,11 @@ int gnugol_plugin_gscrape(argc, argv)
 	dup2(p2c[0], 0); // stdin comes from p2c[0]
 
 #ifdef EXECV
-        execv("inout",NULL);
+        if(execv("/home/d/bin/googd",NULL)>0) {
+		printf("NPG\n"); // No plugin
+	    	fprintf(stderr, "No plugin backend available\n");
+		exit(-1);
+	}
 #else	      
 	while(1) {
         fgets(receive,80,stdin); 
@@ -49,21 +54,33 @@ int gnugol_plugin_gscrape(argc, argv)
 #endif
 	/* never gets here */
         exit(0);
-	      
     }
-    else {                                 /* PARENT process */
-	sprintf(send, "I am your father\n");
-        write(p2c[1],send,strlen(send));
-	i = read(c2p[0],receive,80);       /* get message from child */
-        receive[i] = '\0';
-        fprintf(stderr,"Parent received: %s",receive);
+    p2p[1] = p2c[1];
+    p2p[0] = c2p[0];
+    return(pid);
+}
 
-	sprintf(send, "No, I can't back that up.\n");
-        write(p2c[1],send,strlen(send));
-        i = read(c2p[0],receive,80);       /* get message from child */
-        receive[i] = '\0';
-        fprintf(stderr,"Parent received: %s",receive);
-    }
- 
-    return 0;
-} 
+#ifdef TEST_PLUGIN
+int main(argc, argv)
+     int argc;
+     char *argv[];
+{
+  int i, pid, p2p[2];
+  char send[1280], receive[1280], *gets();
+  if((pid = gnugol_plugin_gscrape_init(p2p)) > 0) { 
+    sprintf(send, "BEGIN LNK SNP\nthis is a test\nEND\n");
+    write(p2p[1],send,strlen(send));
+    i = read(p2p[0],receive,1280);       /* get message from child */
+    receive[i] = '\0';
+    fprintf(stderr,"Parent received: %s",receive);
+    
+    sprintf(send, "BEGIN LNK SNP\nIPv6 address exaustion\nEND\n");
+    write(p2p[1],send,strlen(send));
+    i = read(p2p[0],receive,1280);       /* get message from child */
+    receive[i] = '\0';
+    fprintf(stderr,"Parent received: %s",receive);
+    kill(pid,1);
+  }
+  return 0;
+}
+#endif 
