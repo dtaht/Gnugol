b578a8b642edbf5b1404273c87c556c0d03e4d99
Dave Täht <d@teklibre.org>
2010-12-27 10:50:58 -0800

    Grew tuckered out before I could track down all the licenses
    See credits.c for where I was going with it. I like the idea of
    keyword escapes of the form gnugol:escape_name to control the
    behavior of the engine...

--- src/engines/credits.c
+++ src/engines/credits.c
@@ -34,11 +34,31 @@ static const struct credits c[] = {
   { NULL, NULL, NULL },
 };
 
+static const struct credits copyrights[] = {
+  { "© Michael D. Taht 2010", "http://www.taht.net", "" },
+  { "© Sean Conner 2010", "http://", "" },
+  { NULL, NULL, NULL },
+};
+
+static const struct credits licenses[] = {
+  { "Overview", "http://gnugol.taht.net/", "FIXME: Discussion to be made here" },
+  { "GNU Affero General Public License, Version 3", "http://www.gnu.org/licenses/agpl.html", 
+"The GNU Affero General Public License is a free, copyleft license for software and other kinds of works, specifically designed to ensure cooperation with the community in the case of network server software.\n\n"
+"The GNU Affero General Public License is designed specifically to ensure that the modified source code becomes available to the community. It requires the operator of a network server to provide the source code of the modified version running there to the users of that server. Therefore, public use of a modified version, on a publicly accessible server, gives the public access to the source code of the modified version.\n\n (For the complete license, visit the web site)\n\n" },
+  { "GNU General Public License", "http://www.gnu.org/licenses/gpl.html", "" },
+  { "GNU General Lesser General Public License", "http://www.gnu.org/licenses/lgpl.html", "" },
+  { "Free Beer License", "http://en.wikipedia.org/wiki/Beerware", "" },
+  { NULL, NULL, NULL },
+};
 
 int GNUGOL_DECLARE_ENGINE(setup,credits) (QueryOptions_t *q __attribute__((unused)))
 {
   return 0;
 }
+
+// FIXME: Add keywords of:
+// license, about, credits, source, config, stats, errors, warnings, all
+// FIXME: Add keyword escapes elsewhere, gnugol:whatever
 
 int GNUGOL_DECLARE_ENGINE(search,credits) (QueryOptions_t *q) {
   strcpy(q->keywords,"Gnugol Contributor Credits");
