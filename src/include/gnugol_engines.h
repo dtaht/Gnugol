c5f51ac16b113923162c442e1234223ab528c7d4
Sean Conner <spc@conman.org>
2011-01-07 01:14:51 -0500

    Include nodelist here, and fix a typo.

--- src/include/gnugol_engines.h
+++ src/include/gnugol_engines.h
@@ -1,6 +1,8 @@
 #ifndef _gnugol_engines
 #define _gnugol_engines 1
 
+#include "nodelist.h"
+
 #ifdef DEBUG_SHAREDLIBS
 #  define GNUGOL_SHAREDLIBDIR "../engines"
 #else
@@ -16,7 +18,7 @@ typedef struct ggengine
   int        (*search)(QueryOptions_t *);
 } *GnuGolEngine;
 
-GnuGoldEngine	gnugol_engine_load	(const char *);
+GnuGolEngine	gnugol_engine_load	(const char *);
 int		gnugol_engine_query	(GnuGolEngine,QueryOptions_t *);
 void		gnugol_engine_unload	(GnuGolEngine);
 int		gnugol_read_key		(char *const,size_t *const,const char *const);
